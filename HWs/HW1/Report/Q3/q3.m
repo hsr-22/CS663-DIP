3c022c52eedd2c99eb119a7bdb33ad54c25ca8c5
Harsh Roniyar <112925148+hsr-22@users.noreply.github.com>
2024-08-23 01:43:48 +0530

    q3 done

---
+++ HWs/HW1/Report/Q3/q3.m
@@ -0,0 +1,9 @@
+% Step 1: Load the image
+img = imread('.\..\..\graph.jpeg');
+
+% Step 2: Display the image in a figure
+figure;
+imshow(img);
+
+% Step 3: Enable the impixelinfo tool to display coordinates
+impixelinfo;
