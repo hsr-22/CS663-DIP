bebe94a8549442257ce737426566dde9b994694b
Harsh Roniyar <112925148+hsr-22@users.noreply.github.com>
2024-08-23 00:06:44 +0530

    q5 code with plots done

--- HWs/HW1/Report/Q5/q5.m
+++ HWs/HW1/Report/Q5/q5.m
@@ -24,30 +24,84 @@ for i = 1:length(angles)
     J4 = imrotate(J3, angles(i), 'bilinear', 'crop');
     J4(isnan(J4)) = 0;
     
-    J4 = (J4 - mean(J4(:))) / std(J4(:));
-    J1 = (J1 - mean(J1(:))) / std(J1(:));
+    normJ4 = (J4 - mean(J4(:))) / std(J4(:));
+    normJ1 = (J1 - mean(J1(:))) / std(J1(:));
     
-    ncc(i) = sum(J1(:) .* J4(:)) / numel(J1);
+    ncc(i) = sum(normJ1(:) .* normJ4(:)) / numel(J1);
     
-    jointHist = histcounts2(J1(:), J4(:), 256);
+    jointHist = histcounts2(normJ1(:), normJ4(:), 7); % 256^(1/3) bins approximately
     jointProb = jointHist / sum(jointHist(:));
     je(i) = -sum(jointProb(jointProb > 0) .* log2(jointProb(jointProb > 0)));
     
-    qmi(i) = QMI(J1, J4);
-end
-
-function qmi = QMI(im1, im2)
-    % Quadradic Mutual Information
-
-    bins = 256;
-
-    hist_1 = histcounts(im1, bins);
-    hist_2 = histcounts(im2, bins);
-
-    joint_hist = zeros(bins);
-
-    for i = 1:size(im1, 1)
-        for j = 1:size(im1, 2)
-            joint_hist(im1(i, j) + 1, im2(i, j) + 1) = joint_hist(im1(i, j) + 1, im2(i, j) + 1) + 1;
+    P1 = sum(jointProb, 1);
+    P2 = sum(jointProb, 2);
+    P1P2 = zeros(length(P1));
+    for j = 1:length(P1)
+        for k = 1:length(P2)
+            P1P2(j, k) = P1(j) * P2(k);
         end
     end
+    qmi(i) = sum(sum((jointHist - P1P2).^2));
+end
+
+%%% (c)
+figure; plot(angles, ncc);
+title('NCC vs Angle');
+xlabel('Angle (degrees)');
+ylabel('NCC');
+saveas(gcf, 'NCC_vs_Angle.jpg');
+
+figure; plot(angles, je);
+title('JE vs Angle');
+xlabel('Angle (degrees)');
+ylabel('JE');
+saveas(gcf, 'JE_vs_Angle.jpg');
+
+figure; plot(angles, qmi);
+title('QMI vs Angle');
+xlabel('Angle (degrees)');
+ylabel('QMI');
+saveas(gcf, 'QMI_vs_Angle.jpg');
+
+%%% (d)
+[~, nccIndex] = min(ncc);
+[~, jeIndex] = min(je);
+[~, qmiIndex] = max(qmi);
+
+fprintf('Best NCC angle: %.2f degrees (NCC value: %.4f)\n', angles(nccIndex), ncc(nccIndex));
+fprintf('Best JE angle: %.2f degrees (JE value: %.4f)\n', angles(jeIndex), je(jeIndex));
+fprintf('Best QMI angle: %.2f degrees (QMI value: %.4f)\n', angles(qmiIndex), qmi(qmiIndex));
+
+%%% (e)
+optimJ4 = imrotate(J3, angles(jeIndex), 'bilinear', 'crop');
+optimJ4(isnan(optimJ4)) = 0;
+
+bins = 37;
+
+histJ1 = histcounts(J1(:), bins);
+histJ4 = histcounts(optimJ4(:), bins);
+
+jointHist = zeros(bins);
+
+for i = 1:size(J1, 1)
+    for j = 1:size(optimJ4, 2)
+        intensity_bin1 = floor(J1(i, j) * (bins - 1)) + 1;
+        intensity_bin2 = floor(optimJ4(i, j) * (bins - 1)) + 1;
+        jointHist(intensity_bin1, intensity_bin2) = jointHist(intensity_bin1, intensity_bin2) + 1;
+    end
+end
+
+% Normalize the joint histogram to create a joint probability distribution
+jointProb = jointProb / sum(jointHist(:));
+
+% Plot the joint histogram
+figure; imagesc(jointProb);
+colormap jet;
+colorbar;
+xlabel('Intensity in J4 (Rotated)');
+ylabel('Intensity in J1');
+title('Joint Histogram between J1 and J4 (Optimal JE)');
+axis xy;
+ 
+% Save the plot as an image
+saveas(gcf, 'JointHist_Optimal_JE.jpg');
