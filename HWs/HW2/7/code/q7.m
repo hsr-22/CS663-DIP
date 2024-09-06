abba873f6e71d500d817b7fc68d594c8815f9fe1
Harsh Roniyar <112925148+hsr-22@users.noreply.github.com>
2024-09-07 00:53:01 +0530

    all questions reports as well as solutions done

--- HWs/HW2/7/code/q7.m
+++ HWs/HW2/7/code/q7.m
@@ -6,8 +6,8 @@ imb = im2double(imread('.\..\images\barbara256.png'));
 imk = im2double(imread('.\..\images\kodak24.png'));
 
 % Drawing Original Images
-figure(1); imagesc(imb); colormap("gray"); title("Original barbara256"); saveas(gcf, '.\..\images\barbara256_orig.png');
-figure(2); imagesc(imk); colormap("gray"); title("Original kodak24"); saveas(gcf, '.\..\images\kodak24_orig.png');
+figure(1); imagesc(imb); colormap("gray"); title("Original barbara256"); saveas(gcf, 'barbara256_orig.png');
+figure(2); imagesc(imk); colormap("gray"); title("Original kodak24"); saveas(gcf, 'kodak24_orig.png');
 
 % Zero-mean Gaussian noise
 sig_1 = 5;
@@ -16,8 +16,8 @@ imk_n_1 = imk + sig_1/255*randn(size(imk));
 
 
 % Drawing Images with adding Guassian Noise
-figure(3); imagesc(imb_n_1); colormap("gray"); title("Noisy barbara256 with \sigma_n = " + num2str(sig_1)); saveas(gcf, '.\..\images\barbara256_noise5.png');
-figure(4); imagesc(imk_n_1); colormap("gray"); title("Noisy kodak24 with \sigma_n = " + num2str(sig_1)); saveas(gcf, '.\..\images\kodak24_noise5.png');
+figure(3); imagesc(imb_n_1); colormap("gray"); title("Noisy barbara256 with \sigma_n = " + num2str(sig_1)); saveas(gcf, 'barbara256_noise5.png');
+figure(4); imagesc(imk_n_1); colormap("gray"); title("Noisy kodak24 with \sigma_n = " + num2str(sig_1)); saveas(gcf, 'kodak24_noise5.png');
 
 % Define the parameter configurations
 configurations = [
@@ -35,14 +35,14 @@ for i = 1:length(configurations)
     bil_fil_bar = mybilateralfilter(imb_n_1,config.sig_s, config.sig_r);
     bil_fil_kod = mybilateralfilter(imk_n_1,config.sig_s, config.sig_r);
 
-    filename = [num2str(count),'_barbara256_', num2str(sig_1), '_', num2str(config.sig_s), '_', num2str(config.sig_r), '.png'];
+    filename = ['barbara256_', num2str(sig_1), '_', num2str(config.sig_s), '_', num2str(config.sig_r), '_', num2str(count), '.png'];
     figure(count); imagesc(bil_fil_bar); colormap("gray"); 
-    title("filtered barbara256 with \sigma_n = " + num2str(sig_1) + ", \sigma_s = " + num2str(config.sig_s) + ", \sigma_r = " + num2str(config.sig_r));
+    title("Filtered barbara256 with \sigma_n = " + num2str(sig_1) + ", \sigma_s = " + num2str(config.sig_s) + ", \sigma_r = " + num2str(config.sig_r));
     saveas(gcf, filename);
 
-    filename = [num2str(count+1),'_kodak24_', num2str(sig_1), '_', num2str(config.sig_s), '_', num2str(config.sig_r), '.png'];
+    filename = ['kodak24_', num2str(sig_1), '_', num2str(config.sig_s), '_', num2str(config.sig_r), '_', num2str(count+1), '.png'];
     figure(count+1); imagesc(bil_fil_kod); colormap("gray");
-    title("filtered kodak24 with \sigma_n = " + num2str(sig_1) + ", \sigma_s = " + num2str(config.sig_s) + ", \sigma_r = " + num2str(config.sig_r));
+    title("Filtered kodak24 with \sigma_n = " + num2str(sig_1) + ", \sigma_s = " + num2str(config.sig_s) + ", \sigma_r = " + num2str(config.sig_r));
     saveas(gcf, filename);
     count= count+2;
 
@@ -55,8 +55,8 @@ sig_1 = 10;
 imb_n_1 = imb + sig_1/255*randn(size(imb));
 imk_n_1 = imk + sig_1/255*randn(size(imk));
 
-figure(count); imagesc(imb_n_1); colormap("gray"); title("noisy barbara256 with \sigma_n = " + num2str(sig_1));  saveas(gcf, '11_Noisy_barbara256_10.png');
-figure(count+1); imagesc(imk_n_1); colormap("gray"); title("noisy kodak24 with \sigma_n = " + num2str(sig_1));   saveas(gcf, '12_Noisy_kodak24_10.png');
+figure(count); imagesc(imb_n_1); colormap("gray"); title("noisy barbara256 with \sigma_n = " + num2str(sig_1));  saveas(gcf, 'barbara256_noise10.png');
+figure(count+1); imagesc(imk_n_1); colormap("gray"); title("noisy kodak24 with \sigma_n = " + num2str(sig_1));   saveas(gcf, 'kodak24_noise10.png');
 
 count = count + 2;
 
@@ -67,14 +67,14 @@ for i = 1:length(configurations)
     bil_fil_bar = mybilateralfilter(imb_n_1,config.sig_s, config.sig_r);
     bil_fil_kod = mybilateralfilter(imk_n_1,config.sig_s, config.sig_r);
 
-    filename = [num2str(count), '_barbara256_', num2str(sig_1), '_', num2str(config.sig_s), '_', num2str(config.sig_r), '.png'];
+    filename = ['barbara256_', num2str(sig_1), '_', num2str(config.sig_s), '_', num2str(config.sig_r), '_', num2str(count), '.png'];
     figure(count); imagesc(bil_fil_bar); colormap("gray"); 
-    title("filtered barbara256 with \sigma_n = " + num2str(sig_1) + ", \sigma_s = " + num2str(config.sig_s) + ", \sigma_r = " + num2str(config.sig_r));
+    title("Filtered barbara256 with \sigma_n = " + num2str(sig_1) + ", \sigma_s = " + num2str(config.sig_s) + ", \sigma_r = " + num2str(config.sig_r));
     saveas(gcf, filename);
 
-    filename = [num2str(count+1), '_kodak24_', num2str(sig_1), '_', num2str(config.sig_s), '_', num2str(config.sig_r), '.png'];
+    filename = ['kodak24_', num2str(sig_1), '_', num2str(config.sig_s), '_', num2str(config.sig_r), '_', num2str(count+1), '.png'];
     figure(count+1); imagesc(bil_fil_kod); colormap("gray");
-    title("filtered kodak24 with \sigma_n = " + num2str(sig_1) + ", \sigma_s = " + num2str(config.sig_s) + ", \sigma_r = " + num2str(config.sig_r));
+    title("Filtered kodak24 with \sigma_n = " + num2str(sig_1) + ", \sigma_s = " + num2str(config.sig_s) + ", \sigma_r = " + num2str(config.sig_r));
     saveas(gcf, filename);
     count= count+2;
 
--- HWs/HW2/8/code/q8_global.m
+++ HWs/HW2/8/code/q8_global.m
@@ -1,6 +1,6 @@
 function q8_global()
-    im = imread('.\..\images\LC1.png');
+    im = imread('.\..\images\LC2.jpg');
     im_g = histeq(im);
-    imwrite(im_g, '.\..\images\LC1_global.png');
+    imwrite(im_g, '.\..\images\LC2_global.png');
     %imwrite(f,"LC2_histo_global.jpg");
     figure, imshow(im_g);
