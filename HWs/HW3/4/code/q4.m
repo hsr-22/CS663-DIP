daf178237a41b7f16dc72847b7be9823c5a9833a
Harsh Roniyar <112925148+hsr-22@users.noreply.github.com>
2024-10-07 21:14:21 +0530

    q4 done (interim)

---
+++ HWs/HW3/4/code/q4.m
@@ -0,0 +1,16 @@
+% MATLAB code to compute the Fourier transform
+I = zeros(201, 201); % Create a 201x201 black image
+I(:, 101) = 255; % Set the central column to 255
+
+% Compute the 2D Fourier transform and shift zero-frequency to center
+F = fft2(I);
+F_shifted = fftshift(F);
+
+% Compute the logarithm of the magnitude
+log_magnitude = log(abs(F_shifted) + 1);
+
+% Plot the result
+figure;
+imagesc(log_magnitude);
+colorbar;
+title('Logarithm of Fourier Magnitude');
